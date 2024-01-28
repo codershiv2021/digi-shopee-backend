89e0df0fc4f8ab48a3e4d7fa0a0e2390fb813728
Shivam Kumar <shivamkumarx21@gmail.com>
2024-01-28 19:25:51 +0530

    new

---
+++ routes/auth.js
@@ -0,0 +1,59 @@
+const router = require("express").Router();
+const User = require("../models/User");
+
+//REGISTER
+
+router.post("/register", async(req,res)=>{
+      const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            password:req.body.password
+      });
+      
+      try{
+            const savedUser= await newUser.save(); //
+            res.status(201).json(savedUser);
+      }
+      catch(err){
+            console.log("err");
+            res.status(300).json(err); //chatgpt
+      }
+})
+module.exports=router;
+
+//LOGIN
+
+router.post('/login', async (req, res) => {
+      try {
+          const user = await User.findOne({
+              userName: req.body.user_name
+          });
+  
+          if (!user) {
+              return res.status(401).json("Wrong User Name");
+          }
+  
+          const storedPassword = user.password;
+          const inputPassword = req.body.password;
+  
+          if (storedPassword !== inputPassword) {
+              return res.status(401).json("Wrong Password");
+          }
+
+          const accessToken = jwt.sign(
+            {
+                id: user._id,
+            },
+            process.env.JWT_SEC,
+                {expiresIn:"3d"}
+            );
+      
+            const { password, ...others } = user._doc;  
+            res.status(200).json({...others, accessToken});
+  
+  
+      } catch (error) {
+          console.error('Error during login:', error);
+          res.status(500).json("Internal Server Error");
+      }
+  });
